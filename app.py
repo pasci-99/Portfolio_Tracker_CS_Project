49e6a116f1af5db9943fd78afbb708fe58247c1c
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-11-23 15:03:38 +0100

    a

--- app.py
+++ app.py
@@ -3,27 +3,14 @@ import yfinance as yf
 import pandas as pd
 
 
-st.write("Hooray, we connected everything")
+# enter andd create a stock symbol 
+stock_symbol = st.sidebar.text_input("Enter Stock Symbol (e.g., AAPL):", "AAPL")
 
-st.write("Test")
+# create the ticker with the symbol
+ticker = yf.Ticker(stock_symbol)
 
-
-
-# Function to fetch stock data using yfinance
-def get_stock_data(symbol, interval='1d'):
-    stock = yf.Ticker(symbol)
-    data = stock.history(period=interval)
-    return data
-
-# Streamlit app
-st.title("Stock Price Viewer")
-
-# Sidebar for user input
-symbol = st.sidebar.text_input("Enter Stock Symbol (e.g., AAPL):", "AAPL")
-interval = st.sidebar.selectbox("Select Time Interval:", ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"])
-
-# Fetch and display stock data
-stock_data = get_stock_data(symbol, interval)
-if stock_data is not None:
-    st.write(f"Stock Price Data for {symbol}")
-    st.line_chart(stock_data['Close'])
+# get the wanted data from the ticker
+current_price = ticker.info['ask']
+market_cap = ticker.info['marketCap']
+print(f"The current price of {stock_symbol} is: ${current_price}")
+print(f"The current price of {stock_symbol} is: ${market_cap}")
