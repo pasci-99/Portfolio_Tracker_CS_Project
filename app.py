dcc72ad99a13f55a673cfe19d226883c5d43fe10
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-11-23 15:21:01 +0100

    a

--- app.py
+++ app.py
@@ -3,6 +3,37 @@ import yfinance as yf
 import pandas as pd
 
 
+
+
+# Function to get stock suggestions based on user input
+def get_stock_suggestions(query):
+    suggestions = yf.Ticker(query).suggestions
+    return [s['symbol'] for s in suggestions]
+
+# Streamlit app
+def main():
+    st.title("Stock Comparison App")
+
+    # User input for stock search
+    stock_query = st.text_input("Enter a stock symbol:", "").upper()
+
+    # Get stock suggestions based on user input
+    stock_suggestions = get_stock_suggestions(stock_query)
+
+    # Display stock suggestions in a selectbox
+    selected_stock = st.selectbox("Select a stock:", stock_suggestions)
+
+    # Show the selected stock
+    st.write(f"You selected: {selected_stock}")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
 # enter andd create a stock symbol 
 stock_symbol = st.sidebar.text_input("Enter Stock Symbol (e.g., AAPL):", "AAPL")
 
