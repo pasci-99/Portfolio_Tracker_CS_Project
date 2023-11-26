974ccb95fb7f8825bf0c333107e60c6a40a78e75
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-11-26 17:12:04 +0100

    a

--- app.py
+++ app.py
@@ -1,59 +1,54 @@
 import streamlit as st
+from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
-import matplotlib.pyplot as plt
-import yfinance as yf
-from datetime import datetime
 
-# Initialize an empty DataFrame to store portfolio data
-if 'portfolio' not in st.session_state:
-    st.session_state['portfolio'] = pd.DataFrame(columns=['Stock', 'Quantity', 'Purchase Date'])
+# Hardcoded API key (replace 'YOUR_API_KEY' with your actual API key)
+api_key = ZLEMYDXGU0STLRL1
 
-# Function to add a new stock to the portfolio
-def add_stock(stock_name, quantity, purchase_date):
-    new_entry = pd.DataFrame({'Stock': [stock_name], 'Quantity': [quantity], 'Purchase Date': [purchase_date]})
-    st.session_state['portfolio'] = pd.concat([st.session_state['portfolio'], new_entry], ignore_index=True)
+# Function to fetch stock data
+def get_stock_data(symbol, api_key):
+    ts = TimeSeries(key=api_key, output_format='pandas')
+    data, _ = ts.get_quote_endpoint(symbol=symbol)
+    return data
 
-# Function to plot the portfolio performance
-def plot_performance():
-    if not st.session_state['portfolio'].empty:
-        min_date = st.session_state['portfolio']['Purchase Date'].min()
-        max_date = datetime.now().strftime('%Y-%m-%d')
-        portfolio_performance = pd.DataFrame()
+# Streamlit app layout
+st.title('Stock Portfolio Tracker')
 
-        for index, row in st.session_state['portfolio'].iterrows():
-            stock = yf.Ticker(row['Stock'])
-            hist_data = stock.history(start=min_date, end=max_date)
-            hist_data['Return'] = hist_data['Close'] * row['Quantity']
-
-            # Preparing the DataFrame for addition
-            if portfolio_performance.empty:
-                portfolio_performance = hist_data[['Return']]
-            else:
-                portfolio_performance = portfolio_performance.join(hist_data[['Return']], how='outer', rsuffix='_other')
-                portfolio_performance['Return'] = portfolio_performance['Return'].fillna(0) + portfolio_performance['Return_other'].fillna(0)
-                portfolio_performance.drop(columns=['Return_other'], inplace=True)
-
-        # Plotting
-        if not portfolio_performance.empty:
-            fig, ax = plt.subplots()
-            portfolio_performance['Return'].plot(ax=ax)
-            ax.set_title('Portfolio Performance Over Time')
-            ax.set_ylabel('Value')
-            st.pyplot(fig)
-
-
-# Streamlit application layout
-st.title('Portfolio Tracker')
-
-with st.form("add_stock_form"):
-    stock_name = st.text_input("Stock Symbol")
-    quantity = st.number_input("Quantity", min_value=0.0, format='%f')
-    purchase_date = st.date_input("Purchase Date", value=datetime.now())
+# User input for adding stocks to the portfolio
+with st.form("portfolio_form"):
+    symbol = st.text_input("Enter Stock Symbol (e.g., AAPL)").upper()
+    shares = st.number_input("Enter number of shares", min_value=0, value=0, step=1)
     submitted = st.form_submit_button("Add to Portfolio")
-    if submitted:
-        add_stock(stock_name, quantity, purchase_date)
 
-st.write("Your Portfolio:")
-st.dataframe(st.session_state['portfolio'])
+# Placeholder for displaying the portfolio
+portfolio_container = st.empty()
 
-plot_performance()
+# Store portfolio in a session state to persist data
+if 'portfolio' not in st.session_state:
+    st.session_state.portfolio = pd.DataFrame(columns=['Symbol', 'Shares', 'Price', 'Total Value'])
+
+# If a stock is added
+if submitted and symbol and shares > 0:
+    try:
+        stock_data = get_stock_data(symbol, api_key)
+        price = stock_data['05. price'][0]
+        st.session_state.portfolio = st.session_state.portfolio.append({
+            'Symbol': symbol,
+            'Shares': shares,
+            'Price': price,
+            'Total Value': shares * float(price)
+        }, ignore_index=True)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+# Display the portfolio
+with portfolio_container:
+    if not st.session_state.portfolio.empty:
+        st.write("Your Portfolio:")
+        st.dataframe(st.session_state.portfolio)
+        total_value = st.session_state.portfolio['Total Value'].sum()
+        st.write(f"Total Portfolio Value: ${total_value:.2f}")
+    else:
+        st.write("Your portfolio is empty. Add some stocks to track!")
+
+# Additional features can be added here (like removing stocks, updating prices, etc.)
