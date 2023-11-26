f09f6466d37d180d67bd495d7aaf53683cf45066
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-11-26 17:15:52 +0100

    a

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
+from datetime import datetime
 
 # Hardcoded API key (replace 'YOUR_API_KEY' with your actual API key)
-api_key = ZLEMYDXGU0STLRL1
+api_key = 'YOUR_API_KEY'
 
 # Function to fetch stock data
 def get_stock_data(symbol, api_key):
     ts = TimeSeries(key=api_key, output_format='pandas')
     data, _ = ts.get_quote_endpoint(symbol=symbol)
     return data
+
+# Function to update portfolio value history
+def update_value_history(portfolio, history):
+    if not portfolio.empty:
+        total_value = portfolio['Total Value'].sum()
+        history.loc[datetime.now()] = total_value
+    return history
 
 # Streamlit app layout
 st.title('Stock Portfolio Tracker')
@@ -23,11 +31,13 @@ with st.form("portfolio_form"):
 # Placeholder for displaying the portfolio
 portfolio_container = st.empty()
 
-# Store portfolio in a session state to persist data
+# Initialize session state
 if 'portfolio' not in st.session_state:
     st.session_state.portfolio = pd.DataFrame(columns=['Symbol', 'Shares', 'Price', 'Total Value'])
+if 'value_history' not in st.session_state:
+    st.session_state.value_history = pd.Series(dtype='float64')
 
-# If a stock is added
+# Update portfolio and value history
 if submitted and symbol and shares > 0:
     try:
         stock_data = get_stock_data(symbol, api_key)
@@ -38,6 +48,7 @@ if submitted and symbol and shares > 0:
             'Price': price,
             'Total Value': shares * float(price)
         }, ignore_index=True)
+        st.session_state.value_history = update_value_history(st.session_state.portfolio, st.session_state.value_history)
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
@@ -50,5 +61,9 @@ with portfolio_container:
         st.write(f"Total Portfolio Value: ${total_value:.2f}")
     else:
         st.write("Your portfolio is empty. Add some stocks to track!")
+
+# Display portfolio value history chart
+if not st.session_state.value_history.empty:
+    st.line_chart(st.session_state.value_history)
 
 # Additional features can be added here (like removing stocks, updating prices, etc.)
