02f5662e997795437683e2fd77a891fb163d3a2c
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-11-27 13:38:43 +0100

    a

--- app.py
+++ app.py
@@ -3,67 +3,28 @@ from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 from datetime import datetime
 
-# Hardcoded API key (replace 'YOUR_API_KEY' with your actual API key)
+# API Key, maybe it has to be adjusted to be more secure
 api_key = ZLEMYDXGU0STLRL1
-
-# Function to fetch stock data
-def get_stock_data(symbol, api_key):
-    ts = TimeSeries(key=api_key, output_format='pandas')
-    data, _ = ts.get_quote_endpoint(symbol=symbol)
-    return data
-
-# Function to update portfolio value history
-def update_value_history(portfolio, history):
-    if not portfolio.empty:
-        total_value = portfolio['Total Value'].sum()
-        history.loc[datetime.now()] = total_value
-    return history
+ts = TimeSeries(key=api_key, output_format='pandas')
 
 # Streamlit app layout
-st.title('Stock Portfolio Tracker')
+st.title("Stock Data Fetcher")
 
-# User input for adding stocks to the portfolio
-with st.form("portfolio_form"):
-    symbol = st.text_input("Enter Stock Symbol (e.g., AAPL)").upper()
-    shares = st.number_input("Enter number of shares", min_value=0, value=0, step=1)
-    submitted = st.form_submit_button("Add to Portfolio")
+# User inputs
+symbol = st.text_input("Enter Stock Symbol", "AAPL")
+start_date = st.date_input("Select Start Date")
+start_time = st.time_input("Select Start Time")
+amount = st.number_input("Enter the Amount of Stocks Purchased")
 
-# Placeholder for displaying the portfolio
-portfolio_container = st.empty()
+# Combining date and time into a single datetime object
+start_datetime = datetime.combine(start_date, start_time)
 
-# Initialize session state
-if 'portfolio' not in st.session_state:
-    st.session_state.portfolio = pd.DataFrame(columns=['Symbol', 'Shares', 'Price', 'Total Value'])
-if 'value_history' not in st.session_state:
-    st.session_state.value_history = pd.Series(dtype='float64')
-
-# Update portfolio and value history
-if submitted and symbol and shares > 0:
-    try:
-        stock_data = get_stock_data(symbol, api_key)
-        price = stock_data['05. price'][0]
-        st.session_state.portfolio = st.session_state.portfolio.append({
-            'Symbol': symbol,
-            'Shares': shares,
-            'Price': price,
-            'Total Value': shares * float(price)
-        }, ignore_index=True)
-        st.session_state.value_history = update_value_history(st.session_state.portfolio, st.session_state.value_history)
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-
-# Display the portfolio
-with portfolio_container:
-    if not st.session_state.portfolio.empty:
-        st.write("Your Portfolio:")
-        st.dataframe(st.session_state.portfolio)
-        total_value = st.session_state.portfolio['Total Value'].sum()
-        st.write(f"Total Portfolio Value: ${total_value:.2f}")
-    else:
-        st.write("Your portfolio is empty. Add some stocks to track!")
-
-# Display portfolio value history chart
-if not st.session_state.value_history.empty:
-    st.line_chart(st.session_state.value_history)
-
-# Additional features can be added here (like removing stocks, updating prices, etc.)
+# Fetching data when 'Fetch Data' button is clicked
+if st.button("Fetch Data"):
+    data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+    
+    # Filter data based on the start_datetime
+    data_filtered = data[data.index >= start_datetime]
+    
+    # Display data
+    st.write(data_filtered)
