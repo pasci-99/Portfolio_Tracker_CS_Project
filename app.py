7384d05848643d8c15ffef181052b3719d731c39
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-11-28 15:25:17 +0100

    Portfolio Tracker

--- app.py
+++ app.py
@@ -2,22 +2,22 @@
 import streamlit as st
 from alpha_vantage.timeseries import TimeSeries
 from datetime import datetime
-import pandas as pd
-import matplotlib.pyplot as plt
 
-# API Key (secure it as needed)
+# API Key (maybe make it more secure, not sure yet how to do it)
 api_key = 'ZLEMYDXGU0STLRL1'
 ts = TimeSeries(key=api_key, output_format='pandas')
 
 # Streamlit app layout
-# Title
+#Title
 st.title("Stock Holdings Value Tracker")
 
-# Use a session state to store the holdings
+
+# Use a session state to store the holdings. This is needed, that the user doesn't lose the inserted information when rerunning the app (clicking a button).
+# Using an if clause because only needed if holdings is not existing already
 if 'holdings' not in st.session_state:
     st.session_state['holdings'] = []
 
-# Function to add a holding
+# Function to add a holding (Ticker, Amount, Date, Price)
 def add_holding():
     st.session_state['holdings'].append({
         'symbol': symbol,
@@ -26,70 +26,46 @@ def add_holding():
         'purchase_price': purchase_price
     })
 
-# Function to delete a holding
+# Function to delete an adde holding from the holdings using the pop function
 def delete_holding(index):
     st.session_state['holdings'].pop(index)
 
 # User interface to add a new holding
 with st.form("Add Holding"):
     symbol = st.text_input("Enter Stock Symbol", "AAPL")
-    amount_of_shares = st.number_input("Enter the Number of Shares", min_value=0.1)
+    amount_of_shares = st.number_input("Enter the Number of Shares")
     purchase_date = st.date_input("Select Purchase Date")
-    purchase_price = st.number_input("Enter Purchase Price per Share", min_value=0.1)
+    purchase_price = st.number_input("Enter Purchase Price per Share")
     submitted = st.form_submit_button("Add Holding")
     if submitted:
         add_holding()
 
-# Display current holdings with the option to delete
+# Display current holdings with the option to delete holdings
 for index, holding in enumerate(st.session_state['holdings']):
     st.write(f"Holding {index + 1}: {holding['symbol']} - {holding['amount']} shares")
-    delete_button = st.button(f"Delete Holding {index + 1}", key=f"delete_{index}")
-    if delete_button:
+    if st.button(f"Delete Holding {index + 1}"):
         delete_holding(index)
 
-# Fetch and aggregate data when 'Update Portfolio' button is clicked
+# Fetch and aggregate data when 'Fetch Data' button is clicked
 if st.button("Update Portfolio"):
-    total_values = pd.DataFrame()
+    total_values = None
     for holding in st.session_state['holdings']:
-        try:
-            data, _ = ts.get_daily(symbol=holding['symbol'], outputsize='compact')
-            data_filtered = data[data.index >= holding['purchase_date'].strftime('%Y-%m-%d')]
-            holding_value = data_filtered['4. close'] * holding['amount']
-            total_values[holding['symbol']] = holding_value
-        except Exception as e:
-            st.error(f"Error fetching data for {holding['symbol']}: {e}")
+        # meta_data probably not needed, can still be deleted and outputsize can maybe be adjusted to 'compact' if full length is not needed for a certain holding.
+        data, meta_data = ts.get_daily(symbol=holding['symbol'], outputsize='full')
+
+        # make only dates after the insertet purchasing date True to filter afterwards
+        data_filtered = data[data.index >= holding['purchase_date'].strftime('%Y-%m-%d')]
+        data_filtered['Holdings Value'] = data_filtered['4. close'] * holding['amount']
+       # Needed Help from ChatGPT, thought it would be possible to just do somethin like total_values += data_fil...
+        if total_values is None:
+            total_values = data_filtered[['Holdings Value']]
+        else:
+            total_values = total_values.join(data_filtered[['Holdings Value']], how='outer', rsuffix='_other')
+            total_values['Holdings Value'] = total_values.sum(axis=1)
 
     # Display data as a line chart
-    if not total_values.empty:
-        total_values['Total Value'] = total_values.sum(axis=1)
-        st.line_chart(total_values['Total Value'])
+    if total_values is not None:
+        st.line_chart(total_values['Holdings Value'])
 
         # Display the data in a table format
         st.write(total_values)
-
-# Piechart visualization
-if st.button("Show Portfolio Composition"):
-    current_values = {}
-    total_portfolio_value = 0
-    for holding in st.session_state['holdings']:
-        try:
-            data, _ = ts.get_daily(symbol=holding['symbol'], outputsize='compact')
-            most_recent_close = data['4. close'].iloc[-1]
-            current_value = most_recent_close * holding['amount']
-            current_values[holding['symbol']] = current_value
-            total_portfolio_value += current_value
-        except Exception as e:
-            st.error(f"Error fetching data for {holding['symbol']}: {e}")
-
-    if total_portfolio_value > 0:
-        labels = current_values.keys()
-        sizes = current_values.values()
-
-        plt.figure(figsize=(8, 8))
-        plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-        plt.axis('equal')
-        plt.title('Portfolio Composition')
-
-        st.pyplot(plt)
-    else:
-        st.write("No holdings to display.")
