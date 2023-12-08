e1d3e3c2bdb125d009d3a4e92e1633faa0d877b1
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-12-08 16:49:47 +0100

    test

--- app_3.py
+++ app_3.py
@@ -16,6 +16,29 @@ def add_holding(symbol, amount_of_shares, purchase_date):
     }
     st.session_state['portfolio'].append(holding)
 
+# Function to fetch stock data
+def fetch_stock_data(symbol, start_date):
+    try:
+        stock = yf.Ticker(symbol)
+        hist = stock.history(start=start_date)
+        return hist['Close']
+    except Exception as e:
+        print(f"Error fetching data for {symbol}: {e}")
+        return pd.Series()
+
+# Function to calculate portfolio value
+def calculate_portfolio_value(portfolio):
+    portfolio_df = pd.DataFrame()
+    earliest_date = min(holding['purchase_date'] for holding in portfolio)
+
+    for holding in portfolio:
+        stock_data = fetch_stock_data(holding['symbol'], earliest_date)
+        holding_value = stock_data * holding['shares']
+        holding_value = holding_value.reindex(portfolio_df.index.union(holding_value.index)).fillna(0)
+        portfolio_df = portfolio_df.add(holding_value, fill_value=0)
+
+    return portfolio_df
+
 # Form for adding holdings
 with st.form("Add Holding"):
     symbol_input = st.text_input("Enter Stock Symbol", "AAPL")
@@ -25,6 +48,7 @@ with st.form("Add Holding"):
 
     if submitted:
         add_holding(symbol_input, shares_input, date_input)
+        st.experimental_rerun() # Rerun to refresh the data and display updated portfolio
 
 # Display current portfolio with option to delete each holding
 st.write("Current Portfolio:")
@@ -32,5 +56,9 @@ for i, holding in enumerate(st.session_state['portfolio']):
     st.text(f"{holding['symbol']} - {holding['shares']} shares - Purchased on: {holding['purchase_date']}")
     if st.button("Delete", key=f"delete_{i}"):
         del st.session_state['portfolio'][i]
-        st.experimental_rerun()
+        st.experimental_rerun() # refreshes the page and updates the portfolio.
 
+# Calculate and display portfolio value if the portfolio is not empty
+if st.session_state['portfolio']:
+    portfolio_value = calculate_portfolio_value(st.session_state['portfolio'])
+    st.line_chart(portfolio_value)
