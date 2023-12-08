55433a09654c10280d4800c11e0cba073405aca5
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-12-08 16:26:16 +0100

    test

--- app_3.py
+++ app_3.py
@@ -8,7 +8,7 @@ if 'portfolio' not in st.session_state:
     st.session_state['portfolio'] = []
 
 # Function to store input data as a holding dictionary and append to session state portfolio
-def add_holding():
+def add_holding(symbol, amount_of_shares, purchase_date):
     holding = {
         'symbol': symbol,
         'shares': amount_of_shares,
@@ -16,21 +16,21 @@ def add_holding():
     }
     st.session_state['portfolio'].append(holding)
 
-# Function to delete a holding from the portfolio
-def delete_holding(index):
-    del st.session_state['portfolio'][index]
-
 # Form for adding holdings
 with st.form("Add Holding"):
-    symbol = st.text_input("Enter Stock Symbol", "AAPL")
-    amount_of_shares = st.number_input("Enter the Number of Shares", min_value=0)
-    purchase_date = st.date_input("Select Purchase Date")
-    submitted = st.form_submit_button("Add Holding", on_click=add_holding)
+    symbol_input = st.text_input("Enter Stock Symbol", "AAPL")
+    shares_input = st.number_input("Enter the Number of Shares", min_value=0)
+    date_input = st.date_input("Select Purchase Date")
+    submitted = st.form_submit_button("Add Holding")
+
+    if submitted:
+        add_holding(symbol_input, shares_input, date_input)
 
 # Display current portfolio with option to delete each holding
 st.write("Current Portfolio:")
 for i, holding in enumerate(st.session_state['portfolio']):
     st.text(f"{holding['symbol']} - {holding['shares']} shares - Purchased on: {holding['purchase_date']}")
-    if st.button("Delete", key=i):
-        delete_holding(i)
+    if st.button("Delete", key=f"delete_{i}"):
+        del st.session_state['portfolio'][i]
+        st.experimental_rerun()
 
