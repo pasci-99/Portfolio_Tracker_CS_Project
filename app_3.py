27d2f586a79c20a7b7f7842427c0ed848f904d91
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-12-09 12:01:15 +0100

    supabase

--- app_3.py
+++ app_3.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
+from supabase import create_client, Client
 
 # Streamlit app layout
 st.title("Stock Holdings Value Tracker")
