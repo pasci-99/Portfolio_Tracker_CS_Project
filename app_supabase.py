76ff982ed180d5f620744b8826408168aa254aeb
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-12-10 17:51:50 +0100

    a

--- app_supabase.py
+++ app_supabase.py
@@ -36,3 +36,55 @@ if st.session_state['username'] != "":
 
 
 ).execute())
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+# streamlit_app.py
+
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+
+st_supabase_client = st.connection(
+    name="pasci99connection",
+    type=SupabaseConnection,
+    ttl=None,
+    url="https://pulfkaxpvhgvgvlgjpaj.supabase.co",
+    key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1bGZrYXhwdmhndmd2bGdqcGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIxNTkzNzIsImV4cCI6MjAxNzczNTM3Mn0.twkOSqpf4M7qVREItNHb19rG7iWNli-dtc2DSdEdBlQ",
+)
+
+# Initialize the session state variable if not present
+if 'username' not in st.session_state:
+    st.session_state['username'] = ""
+st.title("Hey " + st.session_state['username'])
+
+
+with st.form("Login"):
+        myUserName = st.text_input("Enter your username")
+        submitted = st.form_submit_button("Login")
+        if submitted:
+            st.session_state['username'] = myUserName
+
+if st.session_state['username'] != "":
+    if st.button("Read"):
+        response = st_supabase_client.query("*", table="test", ttl=0).execute()
+        st.write("Filtered by username:")
+        st.write([obj for obj in response.data if obj.get('username') == myUserName])
+
+    if st.button('Write'):
+        st.write(st_supabase_client.table("test").insert(
+        [{"test": "APPL", "username": st.session_state['username']}], count="None"
+
+
+
+).execute())
