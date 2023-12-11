4fe8703fa024ed5163caa426be60e424ca7a8562
Lorenzo-CHF <148618508+Lorenzo-CHF@users.noreply.github.com>
2023-12-11 22:53:11 +0100

    Update app_supabase.py

--- app_supabase.py
+++ app_supabase.py
@@ -16,7 +16,7 @@ if 'username' not in st.session_state:
 
 # Get the current user's username
 myUserName = st.session_state.get('username')
-col3, col4, space = st.columns(3)
+col3, col4, space, space2 = st.columns(4)
 logo_path = "welink.png"  # Replace with the actual path to your image file
 logo_path = "Portfolio Tracker.jpg"  # Replace with the actual path to your image file
 col3.image(logo_path, width=80, use_column_width=False)
