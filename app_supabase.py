617d803b20569ec052eca387c519123ecbf6e42a
Gianlucacas <153593911+Gianlucacas@users.noreply.github.com>
2023-12-12 11:18:42 +0100

    Update app_supabase.py

--- app_supabase.py
+++ app_supabase.py
@@ -8,17 +8,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 
-# Set the background color to baby blue
-st.markdown(
-    """
-    <style>
-        body {
-            background-color: #89CFF0 !important;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 supabase: Client = create_client("https://pulfkaxpvhgvgvlgjpaj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1bGZrYXhwdmhndmd2bGdqcGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIxNTkzNzIsImV4cCI6MjAxNzczNTM3Mn0.twkOSqpf4M7qVREItNHb19rG7iWNli-dtc2DSdEdBlQ")
 
 # Initialize the session state variable if not present
