cf0c9932158a502b56575b96e3f59c998fad72d6
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-12-13 14:46:49 +0100

    change back

--- app_supabase.py
+++ app_supabase.py
@@ -21,17 +21,6 @@ font="serif"
 if 'username' not in st.session_state:
     st.session_state['username'] = ""
 
-# Logout section
-if myUserName != "":
-    # Logout button (had to move it to the top, since streamlit organizes the webpage based on where the code is in the script)
-    col_logout = st.columns([0.9, 0.1])
-    with col_logout[0]:
-        # Add some space to move the button to the right
-        st.write("")
-    with col_logout[1]:
-        if st.button("Logout"):
-            st.session_state['username'] = ""  # Reset the username in session state
-
 # Get the current user's username
 st.set_page_config(page_title="Login and Elevate Your Portfolio", layout="wide")
 myUserName = st.session_state.get('username')
@@ -41,7 +30,15 @@ logo_path = "Portfolio_Tracker_rundes_Logo-removebg.png"  # Replace with the act
 col3.image(logo_path, width=80, use_column_width=False)
 col4.title("Login and Elevate Your Portfolio " + myUserName)
 
-
+if myUserName != "":
+    # Logout button (had to move it to the top, since streamlit organizes the webpage based on where the code is in the script)
+    col_logout = st.columns([0.9, 0.1])
+    with col_logout[0]:
+        # Add some space to move the button to the right
+        st.write("")
+    with col_logout[1]:
+        if st.button("Logout"):
+            st.session_state['username'] = ""  # Reset the username in session state
 
 # Display brief description
 st.markdown(
