a0ef5f536b158c3f1a3a6e926e2eb84751b8995e
pasci-99 <71435406+pasci-99@users.noreply.github.com>
2023-12-13 14:40:51 +0100

    Reorganize sections

--- app_supabase.py
+++ app_supabase.py
@@ -31,7 +31,7 @@ col3.image(logo_path, width=80, use_column_width=False)
 col4.title("Login and Elevate Your Portfolio " + myUserName)
 
 if myUserName != "":
-    # Logout button 
+    # Logout button (had to move it to the top, since streamlit organizes the webpage based on where the code is in the script)
     col_logout = st.columns([0.9, 0.1])
     with col_logout[0]:
         # Add some space to move the button to the right
@@ -39,6 +39,33 @@ if myUserName != "":
     with col_logout[1]:
         if st.button("Logout"):
             st.session_state['username'] = ""  # Reset the username in session state
+
+    tab1, tab2 = st.tabs(["Portfolio Tracker", "Information Tool"])
+
+    # NewsAPI key
+    news_api_key = 'd3e1cfc10e9e472ca85a16f294c9dc78'
+
+    # Function to fetch news https://newsapi.org/docs
+    def get_stock_news(symbol):
+        base_url = "https://newsapi.org/v2/everything?"
+        query = f"q={symbol}&apiKey={news_api_key}"
+        response = requests.get(base_url + query)
+        articles = response.json().get('articles', [])
+        return articles[:5]  # Return top 5 articles
+
+    # Display news for the selected stock
+    news_symbol = st.sidebar.text_input("Enter Stock Symbol for News")
+    if news_symbol:
+            st.sidebar.write(f"Latest News for {news_symbol}:")
+            news_items = get_stock_news(news_symbol)
+            for item in news_items:
+                    st.sidebar.write(f"**{item['title']}**")
+                    st.sidebar.write(item['description'])
+                    st.sidebar.write(f"[Read more]({item['url']})", unsafe_allow_html=True)
+                    st.sidebar.write("---")
+
+
+    
 
 # Display brief description
 st.markdown(
@@ -50,9 +77,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-if myUserName != "":
-    tab1, tab2 = st.tabs(["Portfolio Tracker", "Information Tool"])
 
 if myUserName == "":
     # region USER IS NOT LOGGED IN
@@ -181,66 +205,54 @@ else:
                     if format_camel_case(key) == formatted_name:
                         return key
                 return None
-            
-
-                # NewsAPI key
-            news_api_key = 'd3e1cfc10e9e472ca85a16f294c9dc78'
-
-            # Function to fetch news https://newsapi.org/docs
-            def get_stock_news(symbol):
-                base_url = "https://newsapi.org/v2/everything?"
-                query = f"q={symbol}&apiKey={news_api_key}"
-                response = requests.get(base_url + query)
-                articles = response.json().get('articles', [])
-                return articles[:5]  # Return top 5 articles
-
-            # Display news for the selected stock
-            news_symbol = st.sidebar.text_input("Enter Stock Symbol for News")
-            if news_symbol:
-                    st.sidebar.write(f"Latest News for {news_symbol}:")
-                    news_items = get_stock_news(news_symbol)
-                    for item in news_items:
-                            st.sidebar.write(f"**{item['title']}**")
-                            st.sidebar.write(item['description'])
-                            st.sidebar.write(f"[Read more]({item['url']})", unsafe_allow_html=True)
-                            st.sidebar.write("---")
-
-
+        
 
 
             # Initialize session state for stock symbols
             if 'num_stocks' not in st.session_state:
                 st.session_state['num_stocks'] = 1  # Start with 1 stock
+
             # Fetch data for a sample stock to get all data points
             sample_stock = yf.Ticker('AAPL')
             sample_data = sample_stock.info
+
             # Extract and format all available data points
             all_data_points = [format_camel_case(point) for point in sample_data.keys()]
+
             # Multiselect dropdown for choosing data points
             selected_data_points = st.multiselect('Select data points for comparison', all_data_points, default=['Current Price', 'Market Cap', 'Sector', 'Dividend Yield', 'Payout Ratio', 'Currency', 'Price To Book'])
+
             # Function to add more stocks
             def add_stock():
                 st.session_state['num_stocks'] += 1
+
             # Button to add more stocks
             st.button('Add another stock', on_click=add_stock)
+
             # Create input fields dynamically based on the number of stocks
             stock_symbols = []
             for i in range(st.session_state['num_stocks']):
                 symbol = st.text_input(f'Stock Symbol {i + 1}', key=f'stock_{i}')
                 if symbol:
                     stock_symbols.append(symbol)
+
             # Fetch data for entered symbols
             stock_data = [fetch_stock_data(symbol) for symbol in stock_symbols if symbol]
+
             # Only proceed if at least one stock has been entered
             if stock_data:
+
                 # Adjust the data fetching to use the original keys
                 comparison_data = {'Data Point': selected_data_points}
                 for symbol, data in zip(stock_symbols, stock_data):
                     comparison_data[symbol] = [data.get(get_original_key(point, sample_data)) for point in selected_data_points]
+
                 # Create the DataFrame
                 comparison_df = pd.DataFrame(comparison_data)
+
                 # Styling the DataFrame
                 st.write(comparison_df.style.set_properties(**{'border-color': 'black', 'border-width': '1px', 'border-style': 'solid'}).set_table_styles([{'selector': 'th', 'props': [('font-size', '16px')]}]))
+
             # Function to fetch YTD stock price data and normalize it
             def fetch_normalize_stock_data(symbol):
                 today = datetime.today().strftime('%Y-%m-%d')
@@ -250,6 +262,7 @@ else:
                 # Normalize to 100 at the starting point
                 normalized_df = (df['Close'] / df['Close'].iloc[0]) * 100
                 return normalized_df
+            
             # Fetch and plot data if at least one stock has been entered
             if stock_data:
                 plt.figure(figsize=(10, 6))
